fea15425093347bc19b516d34bee58374c6b4513
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2019-04-05 14:02:21 +0530

    initial commit
    This is a simple Hello World ReactJS project.

---
+++ helloworld/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/**
+ * Create a functional component App.
+ */
+const App = () => {
+	return (
+		<div>
+			<h1>Hello World</h1>
+		</div>
+	);
+};
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+);
